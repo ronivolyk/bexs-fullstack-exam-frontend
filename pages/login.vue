18c942e8040b43ccce2d31fc918d65fb6f505f9d
Roni Volyk <ronivolyk@gmail.com>
2020-05-14 17:59:34 -0300

    Improving login field to allow login on enter key

--- pages/login.vue
+++ pages/login.vue
@@ -3,7 +3,11 @@
     style="width: 20%; position: fixed; top: 50%; left: 50%; transform: translate(-50%, -50%);"
   >
     <b-field label="Enter your username">
-      <b-input v-model="username" placeholder="Username" />
+      <b-input
+        v-model="username"
+        v-on:keyup.native.enter="login"
+        placeholder="Username"
+      />
     </b-field>
     <b-button @click="login" class="is-success">Log In</b-button>
   </div>
